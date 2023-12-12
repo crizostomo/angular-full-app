c16e5c4917d6710c755e10bb5dbff5e53d39c432
crizostomo <diogo_crizostomo@hotmail.com>
2023-12-12 17:38:18 -0300

    feat: Splitting the Navigation into Components

--- src/app/navigation/sidenav-list/sidenav-list.component.ts
+++ src/app/navigation/sidenav-list/sidenav-list.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-sidenav-list',
   templateUrl: './sidenav-list.component.html',
   styleUrls: ['./sidenav-list.component.css']
 })
-export class SidenavListComponent {
+export class SidenavListComponent implements OnInit {
+  @Output() closeSidenav = new EventEmitter<void>();
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onClose() {
+    this.closeSidenav.emit();
+  }
 
 }
