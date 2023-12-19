c58c9838b419848e50c3054b4b939604186bda5f
crizostomo <diogo_crizostomo@hotmail.com>
2023-12-19 11:30:02 -0300

    feat: Adding a Form to the Training Component

--- src/app/training/training.service.ts
+++ src/app/training/training.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
 import { Exercise } from './exercise.model';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TrainingService {
 
+  exerciseChanged = new Subject<Exercise>();
   private availableExercises: Exercise[] = [
     { id: 'crunches', name: 'Crunches', duration: 30, calories: 8},
     { id: 'touch-toes', name: 'Touch Toes', duration: 180, calories: 15},
@@ -13,8 +15,19 @@ export class TrainingService {
     { id: 'burpees', name: 'Burpees', duration: 60, calories: 8},
   ];
 
+  private runningExercise: Exercise | undefined;
+
   getAvailableExerces() {
     return this.availableExercises.slice(); //slice creates a real copy
+  }
+
+  startExercise(selectedId: string) {
+    const selectedExercise = this.availableExercises.find(ex => ex.id === selectedId);
+
+    if (selectedExercise) {
+      this.runningExercise = selectedExercise;
+      this.exerciseChanged.next({ ...this.runningExercise });
+    }
   }
 
   constructor() { }
