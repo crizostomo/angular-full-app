2bff49fd2b71604a04fb01c1e443edbf6849248f
crizostomo <diogo_crizostomo@hotmail.com>
2024-01-11 17:52:12 -0300

    feat: Listening to Snapshot Changes (of Firestore, including Metadata)

--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -4,25 +4,41 @@ import { Exercise } from '../exercise.model';
 import { NgForm } from '@angular/forms';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-new-training',
   templateUrl: './new-training.component.html',
-  styleUrls: ['./new-training.component.css']
+  styleUrls: ['./new-training.component.css'],
 })
 export class NewTrainingComponent implements OnInit {
-  exercises: Observable<any> | undefined;
+  exercises!: Observable<Exercise[]>;
 
   constructor(
     private trainingService: TrainingService,
     private db: AngularFirestore
-    ) { }
+  ) {}
 
   ngOnInit() {
+    // To mock data
     //this.exercises = this.trainingService.getAvailableExerces();
+
+    // For Firebase Connection
     this.exercises = this.db
-    .collection('availableExercises')
-    .valueChanges();
+      .collection('availableExercises')
+      //.valueChanges()
+      .snapshotChanges()
+      .pipe(
+        map((docArray) => {
+          return docArray.map((doc) => {
+            const data: any = doc.payload.doc.data();
+            return {
+              id: doc.payload.doc.id,
+              ...data,
+            };
+          });
+        })
+      );
     //.subscribe(result => {
     //  console.log(result);
     //});
@@ -31,5 +47,4 @@ export class NewTrainingComponent implements OnInit {
   onStartTraining(form: NgForm) {
     this.trainingService.startExercise(form.value.exercise);
   }
-
 }
