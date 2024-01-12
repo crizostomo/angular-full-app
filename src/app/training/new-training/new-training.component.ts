e6f657b9496fe3784949d7296904bc47e2298ef2
crizostomo <diogo_crizostomo@hotmail.com>
2024-01-11 22:24:46 -0300

    feat: Restructuring the Code

--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { TrainingService } from '../training.service';
 import { Exercise } from '../exercise.model';
 import { NgForm } from '@angular/forms';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { map } from 'rxjs';
 
 @Component({
@@ -11,12 +11,14 @@ import { map } from 'rxjs';
   templateUrl: './new-training.component.html',
   styleUrls: ['./new-training.component.css'],
 })
-export class NewTrainingComponent implements OnInit {
-  exercises!: Observable<Exercise[]>;
+export class NewTrainingComponent implements OnInit, OnDestroy {
+  //exercises!: Observable<Exercise[]>;
+  exercises!: Exercise[] | null;
+  exerciseSubscription!: Subscription;
 
   constructor(
     private trainingService: TrainingService,
-    private db: AngularFirestore
+    //private db: AngularFirestore
   ) {}
 
   ngOnInit() {
@@ -24,27 +26,33 @@ export class NewTrainingComponent implements OnInit {
     //this.exercises = this.trainingService.getAvailableExerces();
 
     // For Firebase Connection
-    this.exercises = this.db
-      .collection('availableExercises')
-      //.valueChanges()
-      .snapshotChanges()
-      .pipe(
-        map((docArray) => {
-          return docArray.map((doc) => {
-            const data: any = doc.payload.doc.data();
-            return {
-              id: doc.payload.doc.id,
-              ...data,
-            };
-          });
-        })
-      );
+    //this.exercises = this.db
+    //  .collection('availableExercises')
+    //  //.valueChanges()
+    //  .snapshotChanges()
+    //  .pipe(
+    //    map((docArray) => {
+    //      return docArray.map((doc) => {
+    //        const data: any = doc.payload.doc.data();
+    //        return {
+    //          id: doc.payload.doc.id,
+    //          ...data,
+    //        };
+    //      });
+    //    })
+    //  );
     //.subscribe(result => {
     //  console.log(result);
     //});
+    this.exerciseSubscription = this.trainingService.exercisesChanged.subscribe(exercises => (this.exercises = exercises));
+    this.trainingService.getAvailableExerces();
   }
 
   onStartTraining(form: NgForm) {
     this.trainingService.startExercise(form.value.exercise);
+  }
+
+  ngOnDestroy() {
+    this.exerciseSubscription.unsubscribe();
   }
 }
--- src/app/training/training.service.ts
+++ src/app/training/training.service.ts
@@ -1,25 +1,52 @@
 import { Injectable } from '@angular/core';
 import { Exercise } from './exercise.model';
 import { Subject } from 'rxjs';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
+
+@Injectable()
 export class TrainingService {
 
   exerciseChanged = new Subject<Exercise | null>();
+  exercisesChanged = new Subject<Exercise[] | null>();
   private availableExercises: Exercise[] = [
-    { id: 'crunches', name: 'Crunches', duration: 30, calories: 8},
-    { id: 'touch-toes', name: 'Touch Toes', duration: 180, calories: 15},
-    { id: 'side-lunges', name: 'Side Lunges', duration: 120, calories: 18},
-    { id: 'burpees', name: 'Burpees', duration: 60, calories: 8},
+  //  { id: 'crunches', name: 'Crunches', duration: 30, calories: 8},
+  //  { id: 'touch-toes', name: 'Touch Toes', duration: 180, calories: 15},
+  //  { id: 'side-lunges', name: 'Side Lunges', duration: 120, calories: 18},
+  //  { id: 'burpees', name: 'Burpees', duration: 60, calories: 8},
   ];
-
   private runningExercise: Exercise | null = null;
   private exercises: Exercise[] = [];
 
+  constructor(
+    private db: AngularFirestore
+    ) {}
+
   getAvailableExerces() {
-    return this.availableExercises.slice(); //slice creates a real copy
+    //return this.availableExercises.slice(); //slice creates a real copy
+
+    this.db
+      .collection('availableExercises')
+      //.valueChanges()
+      .snapshotChanges()
+      .pipe(
+        map((docArray) => {
+          return docArray.map((doc) => {
+            const data: any = doc.payload.doc.data();
+            return {
+              id: doc.payload.doc.id,
+              ...data,
+            };
+          });
+        })
+      ).subscribe((exercises: Exercise[]) => {
+        this.availableExercises = exercises;
+        this.exercisesChanged.next([...this.availableExercises]);
+      });
   }
 
   startExercise(selectedId: string) {
@@ -66,5 +93,4 @@ export class TrainingService {
     return this.exercises.slice(); //slice to get a new copy
   }
 
-  constructor() { }
 }
