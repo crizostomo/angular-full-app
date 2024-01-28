92a48ef1cf9c00a780fad18432d8b54fb4256f5f
crizostomo <diogo_crizostomo@hotmail.com>
2024-01-28 20:20:10 -0300

    feat: Adding prod environment

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,15 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyA53Q4IeU2fPk3Rtk67aDzTrMYVdR6BfmI",
+    authDomain: "ng-fitness-tracker-f821a.firebaseapp.com",
+    databaseURL: "https://ng-fitness-tracker-f821a-default-rtdb.firebaseio.com",
+    projectId: "ng-fitness-tracker-f821a",
+    storageBucket: "ng-fitness-tracker-f821a.appspot.com",
+    messagingSenderId: "87975363929",
+    appId: "1:87975363929:web:3f382296ee67b8ef197848",
+    measurementId: "G-188L8B9GE5"
+  }
+};
+
+// ng build --prod
